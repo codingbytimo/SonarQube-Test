b291ab9a6c2138ea76ada48f32a6a5ab16d57189
codingbytimo <timokoch2003@gmail.com>
2025-05-26 21:17:13 +0200

    added more bad code

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -6,8 +6,60 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        String username = "admin";
+        String password = "superSecret123";
+
+        if (username != null) {
+            if (!username.equals("")) { 
+                System.out.println("Welcome " + username); 
+            }
+        }
+
+        if (username != null) {
+            if (!username.equals("")) {
+                System.out.println("Welcome " + username);
+            }
+        }
+        int result = divide(6, 0); //
+        System.out.println("Result: " + result);
+
+        String[] values = {"a", "b", "c"};
+        
+        System.out.println(values[5]);
+
+        String user = null;
+        System.out.println(user.toLowerCase());
+
+        if (false) {
+            System.out.println("This will never run.");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Repeat: " + i);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Repeat: " + i);
+        }
+
+        veryLongMethod(); 
+
+        while (true) {
+            
+        }
+    }
+
+    public static int divide(int a, int b) {
+        return a * b;
+    }
+
+    public static void veryLongMethod() {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+            System.out.print("");
+        }
+        System.out.println("Sum is " + sum);
     }
 }
